d4deff20418385d53bdc31ef7a53503fd0d0e412
Yoann Deblois <yoann.deblois@gmail.com>
2021-06-18 10:28:16 +0200

    Encoding ok, changed parsing page_content.content instead of page_content.text

--- BTS_Spy.py
+++ BTS_Spy.py
@@ -1,71 +1,118 @@
-# Better to have Phyton > 3.2.2 for decent html.parser (if using html.parser)
+# Better to have Python > 3.2.2 for decent html.parser (if using html.parser)
 
 import requests
 from bs4 import BeautifulSoup
 
 # define BookToScrape URL
 bts_url = "http://books.toscrape.com"
+bts_url_catalogue = bts_url + "/catalogue"
 # defining page URL
-product_page_url = "http://booksss.toscrape.com/catalogue/dune-dune-1_151/index.html"
+cat_name = "travel_2"
+cat_url = "http://books.toscrape.com/catalogue/category/books/" + cat_name + "/index.html"
+# Todo mise en page selon la norme PEP8
+# Todo change reviews in numbers
+# Todo download images
 
-# Todo test with html5lib for £ errors
 
-# extracting page from url and testing connexion
-while True:
-    page_content = requests.get(product_page_url)
-    if page_content.ok:
-        break
+# extracting page from url, testing connexion and make soup
+def extract_soup(url):
+    while True:
+        page_content = requests.get(url)
+        if page_content.ok:
+            break
+        else:
+            print("HTTP Error : ", page_content)
+            user_choice = input("Press Enter to retry, Q to quit program: ")
+            if user_choice.capitalize() == "Q":
+                exit()
+    page_soup = BeautifulSoup(page_content.content, "html.parser")
+    return page_soup
+
+
+# Calculates a superior round
+def ceil(number):
+    if number % 1 == 0 :
+        number=int(number)
     else:
-        print("HTTP Error : ",page_content)
-        user_choice = input("Press Enter to retry, Q to quit program: ")
-        if user_choice.capitalize()=="Q":
-            exit()
+        number = int(number) + 1
+    return number
 
 
-# print(page_content.text) # For test purpose
-
-# Parsing page with BeautifulSoup and html.parser
-page_soup = BeautifulSoup(page_content.text, "html.parser")
-# Selecting Title, extract from list and isolate text
-title = page_soup.select_one('h1').text
-
-# Selecting "Product Information" Table td content and store it in a list
-table = page_soup.select('table.table-striped tr td')
-# Extracting wanted informations from table list
-universal_product_code = table[0].text
-price_including_tax = table[3].text[1:] # Strangely the first character is a bug probably from some encoding mystery
-price_excluding_tax = table[2].text[1:]
-number_available = table[5].text.replace("In stock (","").replace(" available)","")
-
-# Selecting product_description
-# selects the next sibling after the div with id=product-description
-product_description = '"' + page_soup.select_one("#product_description ~ p").text + '"'
-
-# Selecting category
-category = page_soup.select_one('.breadcrumb > li:nth-of-type(3) > a').text
-
-# Extracting review_rating
-review_rating = page_soup.select_one('.star-rating').get('class')[1] # todo Poser la question : Pourquoi .p ne marche pas ?
-
-# Extracting img relative url "src=", truncate and concatenate with bts_url to form complete URL
-image_url = bts_url + page_soup.select_one("#product_gallery .item").img['src'][5:]
+# Calculates the number of pages in a category
+# and generates a list of pages
+def list_of_pages_in_category(cat_url, cat_name):
+    book_number = extract_soup(cat_url).select_one('.form-horizontal > strong').text
+    nb_pages = ceil(int(book_number) / 20)
+    cat_urls_list = [cat_url]
+    for nb_page in range(2, nb_pages+1):
+        cat_urls_list.append(bts_url_catalogue + "/category/books/" + cat_name + "/page-" + str(nb_page) + ".html"  )
+    return cat_urls_list
 
 
-# test print
-# todo remove test print section
-print("product_page_url : ", product_page_url)
-print("Universal_product_code : ", universal_product_code)
-print("title : ", title)
-print("price_including_tax : ", price_including_tax)
-print("price_excluding_tax : ", price_excluding_tax)
-print("number_available : ", number_available)
-print("product_description : ", product_description)
-print("category : ", category)
-print("review_rating : ", review_rating)
-print("image_url : ", image_url)
-# end of test print
+# find all book links in a category
+def book_links(cat_urls_list):
+    raw_links = []
+    for page in range(0, len(cat_urls_list)):
+        page_soup = extract_soup(cat_urls_list[page])
+        raw_links = raw_links + page_soup.select('h3 > a')
+    clean_links = []
+    for raw_link in raw_links:
+        clean_links.append(bts_url_catalogue + raw_link['href'][8:])
+    return clean_links
+
+
+# Extracting info in book page
+def extract_info(product_page_url, bts_url):
+    page_soup = extract_soup(product_page_url)
+    # Selecting Title, extract from list and isolate text and format with quotes
+    title = '"' + page_soup.select_one('h1').text.replace('"', '') + '"'
+    # Selecting "Product Information" Table td content and store it in a list
+    table = page_soup.select('table.table-striped tr td')
+    # Extracting wanted informations from table list
+    universal_product_code = table[0].text
+    price_including_tax = table[3].text# Strangely the first character is a bug probably from some encoding mystery
+    price_excluding_tax = table[2].text
+    number_available = table[5].text.replace("In stock (", "").replace(" available)", "")
+    print(price_including_tax)
+    print(price_excluding_tax)
+    # Selecting product_description
+    # selects the next sibling after the div with id=product-description
+    product_description = page_soup.select_one("#product_description ~ p")
+    print(product_description)
+    # Checks if product description exists ; if no, replace with something, if yes format for CSV
+    if product_description is None:
+        product_description ='"Non renseigné"'
+    else:
+        product_description = '"' + product_description.text.replace('"', '').replace(' ...more', '') + '"'
+    # Selecting category
+    category = page_soup.select_one('.breadcrumb > li:nth-of-type(3) > a').text
+
+    # Extracting review_rating
+    review_rating = page_soup.select_one('.star-rating').get('class')[1]  # todo Poser la question : Pourquoi .p ne marche pas ?
+
+    # Extracting img relative url "src=", truncate and concatenate with bts_url to form complete URL
+    image_url = bts_url + page_soup.select_one("#product_gallery .item").img['src'][5:]
+    # Making list for one book
+    list_info = (product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url)
+    return list_info
+
+def init_csv(cat_name):
+    with open(cat_name + '.csv', 'w') as cat_name:
+        print("product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url", file=cat_name)
+
+def append_csv(cat_name, list_info):
+    with open(cat_name + '.csv', 'a') as cat_name:
+        print(list_info[0] + "," + list_info[1] + "," + list_info[2]
+              + "," + list_info[3] + "," + list_info[4] + ","
+              + list_info[5] + "," + list_info[6]
+              + "," + list_info[7] + "," + list_info[8] + ","
+              + list_info[9], file=cat_name)
+
+
+links = book_links(list_of_pages_in_category(cat_url, cat_name))
+init_csv(cat_name)
+for link in links:
+    append_csv(cat_name, extract_info(link, bts_url))
 
 # CSV Generation todo save in a folder
-with open('dune.csv', 'w') as dunecsv:
-    print("product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url", file=dunecsv)
-    print(product_page_url + "," + universal_product_code + "," + title + "," + price_including_tax + "," + price_excluding_tax + "," + number_available + "," + product_description + "," + category + "," + review_rating + "," + image_url, file=dunecsv)
+
